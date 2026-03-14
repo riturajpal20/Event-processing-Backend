4644ea84330256f6bf2e4bee4f3d53fbb3409241
riturajpal-kas <rituraj@kascommerce.com>
2026-03-14 14:29:28 +0530

    first commit

---
+++ workers.py
@@ -0,0 +1,53 @@
+import asyncio
+import time
+
+from backend.queue_manager import event_queue
+from backend.metrics import metrics
+
+
+async def process_event(event):
+
+    start = time.perf_counter()
+
+    revenue = event["price"] * event["quantity"]
+    category = event["category"]
+
+    processing_time = (time.perf_counter() - start)*1000
+
+    async with metrics.lock:
+
+        metrics.events_processed += 1
+        metrics.revenue += revenue
+
+        # update revenue by category
+        metrics.revenue_by_category.setdefault(category, 0)
+        metrics.revenue_by_category[category] += revenue
+
+        # update processing latency (moving average)
+        metrics.avg_processing_latency = (
+            metrics.avg_processing_latency * 0.9
+            + processing_time * 0.1
+        )
+
+
+async def worker(worker_id):
+
+    while True:
+
+        event = await event_queue.get()
+
+        await process_event(event)
+
+        event_queue.task_done()
+
+
+async def start_workers(count):
+
+    tasks = []
+
+    metrics.worker_count = count   # NEW
+
+    for i in range(count):
+        tasks.append(asyncio.create_task(worker(i)))
+
+    return tasks
